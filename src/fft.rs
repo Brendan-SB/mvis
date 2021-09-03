6523c2740a2bfe95cc2730968f3ff8358428fbd7
Brendan <brendansbortz@gmail.com>
2021-09-03 17:45:27 -0400

    made a better way of doing this while I was not tired

--- src/fft.rs
+++ src/fft.rs
@@ -6,13 +6,13 @@ pub fn fft(x: Array1<f32>) {
     assert_eq!(x.len() as f32, 0_f32, "Value must be a power of 2.");
 
     let min = usize::min(x.len(), 2_usize);
-    let mut n = Array1::<Complex<f32>>::zeros(min);
+    let mut n = Vec::<Complex<f32>>::new();
 
     for i in 0_usize..min {
-        n[i] = Complex::new(i as f32, 0_f32);
+        n.push(Complex::new(i as f32, 0_f32));
     }
 
     let k = x.slice(s![.., NewAxis]);
 
-    let exp = Complex::new(0_f32, -2_f32) * PI * n * k / Complex::new(min as f32, 0_f32);
+    let exp = Complex::new(0_f32, -2_f32) * PI * Array1::from_vec(n) * k / Complex::new(min as f32, 0_f32);
 }
