129cdeae6d95f9b579fdfd3975d7750bf3b08ee1
Brendan <brendansbortz@gmail.com>
2021-09-02 22:35:47 -0400

    finally figured this out. Not the best, but it is better than before

--- src/fft.rs
+++ src/fft.rs
@@ -1,27 +1,18 @@
-#[macro_export]
-macro_rules! fft {
-    ($x:expr) => {};
-}
-
-use ndarray::{s, Array, Array1, NewAxis};
+use ndarray::{s, Array1, NewAxis};
 use num_complex::Complex;
 use std::f32::consts::PI;
 
 pub fn fft(x: Array1<f32>) {
-    let x_size = x.len() as f32;
+    assert_eq!(x.len() as f32, 0_f32, "Value must be a power of 2.");
 
-    assert_eq!(
-        x_size.log(2_f32) % 1_f32,
-        0_f32,
-        "Value must be a power of 2."
-    );
+    let min = usize::min(x.len(), 2_usize);
+    let mut n = Array1::<Complex<f32>>::zeros(min);
 
-    let n = Array::range(0_f32, f32::min(x_size, 2_f32), 1_f32)
-        .into_iter()
-        .map(|x| Complex::new(x, 0_f32))
-        .collect::<Array1<Complex<f32>>>();
+    for i in 0_usize..min {
+        n[i] = Complex::new(i as f32, 0_f32);
+    }
 
     let k = x.slice(s![.., NewAxis]);
 
-    let exp = Complex::new(0_f32, -2_f32) * PI * n;
+    let exp = Complex::new(0_f32, -2_f32) * PI * n * k / Complex::new(min as f32, 0_f32);
 }
