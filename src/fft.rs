769c1dcb44bd3b0b796a5553d127202df09b7448
Brendan <brendansbortz@gmail.com>
2021-09-02 21:47:16 -0400

    ndarray is confusing for my tired brain

--- src/fft.rs
+++ src/fft.rs
@@ -1,12 +1,11 @@
 #[macro_export]
 macro_rules! fft {
-    ($x:expr) => {
-    };
+    ($x:expr) => {};
 }
 
-use std::f32::consts::PI;
 use ndarray::{s, Array, Array1, NewAxis};
 use num_complex::Complex;
+use std::f32::consts::PI;
 
 pub fn fft(x: Array1<f32>) {
     let x_size = x.len() as f32;
@@ -17,8 +16,12 @@ pub fn fft(x: Array1<f32>) {
         "Value must be a power of 2."
     );
 
-    let n = Array::range(0_f32, 1_f32, f32::min(x_size, 2_f32));
+    let n = Array::range(0_f32, f32::min(x_size, 2_f32), 1_f32)
+        .into_iter()
+        .map(|x| Complex::new(x, 0_f32))
+        .collect::<Array1<Complex<f32>>>();
+
     let k = x.slice(s![.., NewAxis]);
 
-    let m = Complex::new(0_f32, -2_f32) * PI;
+    let exp = Complex::new(0_f32, -2_f32) * PI * n;
 }
