39766753c4b78610cff2c97fd186764aebab0923
Brendan <brendansbortz@gmail.com>
2021-09-06 19:41:36 -0400

    ok, ok, im finally getting somewhere

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use kira::{
     manager::{AudioManager, AudioManagerSettings},
     sound::SoundSettings,
 };
-use std::{thread::sleep, time::Duration};
+use std::{cell::RefCell, thread::sleep, time::Duration};
 
 fn main() {
     Config::try_create_default_config();
@@ -27,9 +27,14 @@ fn main() {
     let sound_handle = audio_manager
         .load_sound(&config.audio_file_path, SoundSettings::default())
         .unwrap();
-    let mut arrangement = Arrangement::new(ArrangementSettings::new());
-    
+
+    let mut arrangement = Arrangement::new(ArrangementSettings::default());
+
     arrangement.add_clip(SoundClip::new(&sound_handle, 0_f64).trim(20_f64));
-    
+
+    let arrangement_handle = audio_manager.add_arrangement(
+        arrangement
+    );
+
     sleep(Duration::from_secs_f64(sound_handle.duration()));
 }
