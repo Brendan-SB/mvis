2aedbdc9855e9b0326efb478da82c6944c20db6d
Brendan <brendansbortz@gmail.com>
2021-09-16 10:00:40 -0400

    thing

--- src/update_display.rs
+++
@@ -1,7 +0,0 @@
-use num_complex::Complex;
-
-pub fn update_display(data: &[Complex<f32>]) {
-    for (i, v) in data.iter().enumerate() {
-        let dist = (v.re * v.re + v.im * v.im).sqrt();
-    }
-}
