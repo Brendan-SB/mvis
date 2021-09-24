4afede13e7abb6f28324d4284928da859f91a6d8
Brendan Bortz <brendansbortz@gmail.com>
2021-09-23 23:49:59 -0400

    fits in one window

--- src/display.rs
+++ src/display.rs
@@ -21,18 +21,6 @@ impl<'a> Display<'a> {
         Self { config, terminal }
     }
 
-    fn compute_offset(terminal_width: f64, bar_width: f64, data_dist: &Vec<u64>) -> usize {
-        let offset = (terminal_width as f64
-            / (bar_width as f64 * data_dist.len() as f64))
-            .round() as usize;
-
-        if offset == 0 {
-            return 1;
-        }
-
-        offset
-    }
-
     pub fn update(&mut self, data: &[Complex<f32>]) {
         let bar_width = self.config.bar_width;
         let terminal_width = self.terminal.size().unwrap().width as usize;
@@ -47,7 +35,12 @@ impl<'a> Display<'a> {
                         .map(|x| ((x.re * x.re + x.im * x.im).round() as u64))
                         .collect::<Vec<u64>>();
 
-                    let offset = Self::compute_offset(terminal_width as f64, bar_width as f64, &data_dist);
+                    let bar_width_f64 = bar_width as f64;
+
+                    let offset = (((bar_width_f64 * (data_dist.len() + 1) as f64)
+                        + bar_width_f64 * data_dist.len() as f64)
+                        / terminal_width as f64).round() as usize;
+
                     for i in (0..data_dist.len() - offset).step_by(offset) {
                         let mut sum = 0;
 
