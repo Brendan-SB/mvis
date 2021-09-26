77cb640af273675d07c0985daf789ed7a1441e71
Brendan <brendansbortz@gmail.com>
2021-09-26 10:56:25 -0400

    can now download stuff from youtube

---
+++ src/clean_tmp_dir.rs
@@ -0,0 +1,20 @@
+use std::{fs, path::Path};
+
+pub fn clean_tmp_dir<P: AsRef<Path>>(path: P) -> Result<(), std::io::Error> {
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if entry.file_type()?.is_dir() {
+            let result = clean_tmp_dir(&path);
+
+            fs::remove_dir(path)?;
+
+            return result;
+        } else {
+            fs::remove_file(path)?;
+        }
+    }
+
+    Ok(())
+}
